7b19d2cbc5010da1fbf357e8d26844e3ac7b8388
LDenninger <Luis0512@web.de>
2023-04-23 17:29:45 +0200

    further functionalities for the logger

--- utils/logger.py
+++ utils/logger.py
@@ -9,6 +9,7 @@ import optuna
 
 import os
 import copy
+from collections import OrderedDict
 
 from .management import *
 
@@ -24,6 +25,7 @@ class Logger():
                  checkpoint_frequency: int = 0,
                  disabled: bool = False,
                  anon_mode: bool = False,
+                 log_internal: bool = True,
                  init_run: bool = False,
                  ):
         
@@ -33,13 +35,25 @@ class Logger():
         self.log_data = log_data
         self.checkpoint_frequency = checkpoint_frequency
 
-        self._internal_save = {}
+        # Dictionary for internal logging
+        self._internal_log = OrderedDict()
+
+        # Some internal flags to detect how the logger is currently used
+        self._run_init = False
+        self._training = False
+        self._evaluation = False
+        self._tuning = False
+        self._dir_set = False
+
+        self.log_internal = log_internal
 
         # The logger does not log at all 
         self.disabled = disabled
 
         # In anonymous mode, the logger does not write to disk but rather saves the data internally to be retrieved later from the user.
         self.anon_mode = anon_mode
+
+        self.log_internal
 
         self.epoch = 1
         self.iteration = 1
@@ -72,8 +86,14 @@ class Logger():
             self.exp_name = None
             self.run_name = None
             self.anon_mode = True
+        if self.exp_name is not None and self.run_name is not None:
+            self._init_run = True
         if self.anon_mode:
             print('Run environement not completely specified. Logger running in anonymous mode --> No logging to disk.')
+
+        if init_run:
+            self.init_run()
+
         else:
             if "EXP_DIRECTORY" in os.environ:
                 self.exp_dir = P(os.environ["EXP_DIRECTORY"]) / 'exp_data' / exp_name
@@ -81,9 +101,13 @@ class Logger():
                 self.anon_mode = True
                 print("Experiment directory not set. Please set the environment variable EXP_DIRECTORY.\n Logger running in anonymous mode --> No logging to disk.")
             self.exp_dir = P('exp_data') / exp_name
-        self.run_dir = self.exp_dir / run_name
-        
+            self._dir_set = True
+            self.run_dir = self.exp_dir / run_name
 
+            if not os.path.exists(str(self.run_dir)):
+                print('Run directory does not exist.')
+        
+        
         self.model_config = model_config
 
         ### Tensorboard Logger ###
@@ -91,22 +115,86 @@ class Logger():
 
         ### Initialize Data Solver ###
 
-
-
-        if self.verbose:
-            output = ''
-            output += f' ------Start Training of run {self.run_name}-----\n\n'
-            output += f' --Logger Configuration--\n  Log Gradients: {self.log_gradients}\n  Log Data: {self.log_data}\n  Checkpoint Frequency: {self.checkpoint_frequency}\n  Verbose: {self.verbose}\n'
-            output += f'\n --Model Configuration--\n'
-            for key, value in self.model_config.items():
-                output += f'  {key}: {value}\n'
-            print(output)
-        if init_run:
-            self.init_run()
+        
 
     def init_run(self):
         initiate_experiment(self.exp_name)
         initiate_run(self.run_name)
+    
+    def init_training(self, id: str = None):
+        """
+            Initial function call to notify the logger that it is about to being used.
+        
+        """
+
+        if self.disabled:
+            print('Logger is disabled --> Cannot start logger.')
+            return 2
+      
+        # Set flags 
+        self._training = True
+        self._evaluation = False
+        self._tuning = False
+        if self.exp_name is not None and self.run_name is not None:
+            self._init_run = True
+        if self.exp_dir is not None:
+            self._dir_set = True
+        
+        self.epoch = 0
+        self.iteration = 0
+
+        if id is not None:
+            assert id not in self._internal_save.keys(), 'Please choose a unique identifier for this run.'
+        else:
+            id = 'auto_' + str(len(self._internal_log.keys())).zfill(4) if self.run_name is None else self.run_name + str(len(self._internal_log.keys())).zfill(4)
+
+        self.curr_id = id
+        self._internal_log[id] = {}
+
+        print('Logger was initialized for training.')
+        if self.verbose:
+            self._print_logger_config()
+    
+    def finish(self):
+        if not self._training and not self._evaluation and not self._tuning:
+            print('Nothing to finish, nothing started yet.')
+        
+        if self._training:
+            self.epoch = self.model_config['num_epochs']
+            self.iteration = 0
+            print('Logging of training finished. Continue with evaluation...')
+            return self.init_evaluation(id=(self.curr_id + '_eval'))
+        
+        self.curr_id = None
+        self.epoch = 0
+        self.iteration = 0
+        self._training = False
+        self._evaluation = False
+        self._tuning = False
+    
+    def init_evaluation(self, id: str = None):
+        if self.disabled:
+            print('Logger is disabled --> Cannot start logger.')
+            return 2
+        
+     
+        
+        self._evaluation = True
+        self._tuning = False
+        self._training = False
+
+        if id is not None:
+            assert id not in self._internal_save.keys(), 'Please choose a unique identifier for this run.'
+        else:
+            id = 'auto_' + str(len(self._internal_log.keys())).zfill(4) if self.run_name is None else self.run_name + str(len(self._internal_log.keys())).zfill(4)
+
+
+        self.curr_id = id
+
+        print('Logger was initialized for evaluation.')
+        if self.verbose:
+            self._print_logger_config()
+
 
     def step(self,
             epoch: int = None,
@@ -117,29 +205,29 @@ class Logger():
             iteration: int = None):
         
         """
-        Logger takes a step. The provided data is saved to tensorboard files if the logger is set up to save data.
-        The logger automatically takes checkpoints in the frequency defined by the configuration.
+            Logger takes a step in the training process. The provided data is saved to tensorboard files if the logger is set up to save data.
+            The logger automatically takes checkpoints in the frequency defined by the configuration.
 
-        Arguments:
-            Epoch (int): Current epoch.
-            data (optional): Data to log. The data should be provided through a dictionary of form:
-            data_resolved (bool, optional): Whether or not the data is already resolved. 
-                                            If the data is not resolved, the logger tries to resolve the data automatically. Defaults to True.
-                Resolved format of data: 
-                    {
-                    'name': value,
-                    }
+            Arguments:
+                Epoch (int): Current epoch.
+                data (optional): Data to log. The data should be provided through a dictionary of form:
+                data_resolved (bool, optional): Whether or not the data is already resolved. 
+                                                If the data is not resolved, the logger tries to resolve the data automatically. Defaults to True.
+                    Resolved format of data: 
+                        {
+                        'name': value,
+                        }
 
-            model (torch.nn.Module, optional): Model to log.
-            optimizer (torch.optim.Optimizer, optional): Optimizer to log.
-            iteration (int, optional): Current iteration.
-        
+                model (torch.nn.Module, optional): Model to log.
+                optimizer (torch.optim.Optimizer, optional): Optimizer to log.
+                iteration (int, optional): Current iteration.
+            
         
         """
 
         ## Step forward to track training progress ##
         #Auto-step forward within logger 
-        if self.iteration == self.model_config['num_iterations']:
+        if self.iteration == self.model_config['num_iterations'] or (self.iteration == 0 and self.epoch == 0):
             self.epoch += 1
             self.iteration = 0
         self.iteration += 1
@@ -153,24 +241,26 @@ class Logger():
             self.iteration = 0
         elif epoch is None and iteration is not None:
             self.iteration = iteration+1
+        
+        if self.verbose and data is not None:
+            output = ''
+            output += f'-----Epoch {epoch} Results -----\n'
+            output += yaml.dump(data, default_flow_style=False)
+            print(output)
 
         if self.disabled:
             return 2
+        
 
         try:
             if data is not None:
-                if self.verbose:
-                    output = ''
-                    output += f'-----Epoch {epoch} Results -----\n'
-                    output += yaml.dump(data, default_flow_style=False)
-                    print(output)
                 if self.log_data:
                     self.log(epoch, data, iteration)
 
             if model is not None and self.log_gradients:
                 self.log_model_gradients(epoch, model, iteration)
 
-            if self.checkpoint_frequency != 0 and (epoch % self.checkpoint_frequency == 0) and iteration is None:
+            if self.model_config['save_frequency']!= 0 and (epoch % self.model_config['save_frequency'] == 0) and iteration is None:
 
                 self.checkpoint(epoch, model, optimizer)
         except:
@@ -200,14 +290,20 @@ class Logger():
 
         if self.disabled:
             return 2
-
+        
         prefix_name = f'epoch_metrics/' if iteration is None else f'iteration_metrics/'
 
         log_iter = self._get_log_iteration(epoch, iteration)
-
-        for key, value in data.items():
-            if value!=-1:
-                self.writer.add_scalar(prefix_name + key, int(value), log_iter)
+        
+        if self.log_internal:
+            data_intern = {}
+            for k, v in data.items():
+                data_intern[(prefix_name+k)] = v
+            self._log_internal(data=data_intern)
+        if not self.anon_mode:
+            for key, value in data.items():
+                if value!=-1:
+                    self.writer.add_scalar(prefix_name + key, int(value), log_iter)
         return 1
     
     def log_model_gradients(self, 
@@ -228,6 +324,7 @@ class Logger():
         if self.disabled:
             return 2
         
+        self._tuning = True
 
         save_dict = {}
         save_dict['base_config'] = self.model_config
@@ -255,7 +352,7 @@ class Logger():
                         'trial_data': data
             }
 
-        self._anonymous_log(id='pOpt', data=save_dict)
+        self._log_internal(type='tuning', id=name, data=save_dict)
         print('Hyperparameter tuning is getting logged internally...')
 
         if not self.anon_mode:
@@ -269,7 +366,8 @@ class Logger():
             
             yaml = yaml.YAML()
             yaml.dump(save_dict, save_path)
-
+    
+        self._tuning = False
         
     
     def checkpoint(self, epoch: int, model: torch.nn.Module, optimizer: torch.optim.Optimizer = None, name: str = None):
@@ -298,10 +396,56 @@ class Logger():
         yaml = yaml.YAML()
         yaml.dump(self.model_config, open(save_path, 'w'))
 
-    def _anonymous_log(self, id: str, data):
+    def _log_internal(self, type: str, data, id: str = None):
+
+
         if self.disabled:
             return 2
-        self._internal_save[id].append(data)
+    
+        if id is None:
+            id = self.curr_id
+
+        if self._tuning:
+            if id is None:
+                i = '001'
+                while ('hyperOpt_' + i) in self._internal_log.keys():
+                    i = str(int(i) + 1).zfill(3)
+                id = 'hyperOpt_' + i
+            if id not in self._internal_log.keys():
+                self._internal_log[id] = {}
+            for k, v in data.items():
+                if k not in self._internal_log[id].keys():
+                    self._internal_log[id][k] = []
+                self._internal_save[id][k].append(v)
+
+        if self._training:
+            if id is None:
+                id = next(reversed(self._internal_log))
+            for k, v in data.items():
+                if k not in self._internal_log[id].keys():
+                    self._internal_log[id][k] = []
+                self._internal_save[id][k].append(v)
+            return 1
+        
+        if self._evaluation:
+            if id is None:
+                i = '001'
+                while ('eval_' + i) in self._internal_log.keys():
+                    i = str(int(i) + 1).zfill(3)
+                id = 'eval_' + i
+            for k, v in data.items():
+                if k not in self._internal_log[id].keys():
+                    self._internal_log[id][k] = []
+                self._internal_save[id][k].append(v)
+            return 1
+ 
+            
+                
+        if type not in self._internal_log[id].keys():
+            self._internal_log[id][type] = []
+        
+        self._internal_save[id][type].append(data)
+
         return 1
 
     def _initialize_data_patterns(self):
@@ -379,3 +523,27 @@ class Logger():
             n = self.epoch*self.model_config['num_iterations'] + iteration
         
         return n
+    
+    def _print_logger_config(self):
+        output = ''
+        output += f' <----Logger Configuration---->\n  Log Gradients: {self.log_gradients}\n  Log Data: {self.log_data}\n  Checkpoint Frequency: {self.checkpoint_frequency}\n  Verbose: {self.verbose}\n'
+        output += f'  Log Gradients: {self.log_gradients}\n'
+        output += f'  Log Data: {self.log_data}\n'
+        sf = self.model_config['save_frequency']
+        output += f'  Checkpoint Frequency: {sf}\n'
+        output += f'  Anonymous Mode: {self.anon_mode}\n'
+        output += f'  Disabled: {self.disabled}\n'
+        output += f'  Verbose: {self.verbose}\n'
+        output += f'  Current Logger State:\n'
+        output += f'    Log ID: {self.curr_id}\n' if self.curr_id is not None else f'    Log ID: -----\n'
+        output += f'    Training: {self._training}\n'
+        output += f'    Evaluation: {self._evaluation}\n'
+        output += f'    Tuning: {self._tuning}\n'
+        output += f'    Epoch: {self.epoch}\n'
+        output += f'    Iteration: {self.iteration}\n'
+        output += f'\n --Model Configuration--\n'
+        for key, value in self.model_config.items():
+            output += f'  {key}: {value}\n'
+        
+        print(output)
+        
